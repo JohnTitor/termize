ef645fbc9b2c30a0e0fd39ad27be6da12f5fd82f
Yuki Okushi <huyuumi.dev@gmail.com>
2019-12-30 14:25:31 +0900

    Tweak target platform

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,61 +1,15 @@
-#[cfg(any(
-    target_os = "linux",
-    target_os = "android",
-    target_os = "macos",
-    target_os = "ios",
-    target_os = "bitrig",
-    target_os = "dragonfly",
-    target_os = "freebsd",
-    target_os = "netbsd",
-    target_os = "openbsd",
-    target_os = "solaris"
-))]
+#[cfg(unix)]
 mod unix;
-#[cfg(any(
-    target_os = "linux",
-    target_os = "android",
-    target_os = "macos",
-    target_os = "ios",
-    target_os = "bitrig",
-    target_os = "dragonfly",
-    target_os = "freebsd",
-    target_os = "netbsd",
-    target_os = "openbsd",
-    target_os = "solaris"
-))]
+#[cfg(unix)]
 pub use self::unix::{dimensions, dimensions_stderr, dimensions_stdin, dimensions_stdout};
 
-#[cfg(target_os = "windows")]
+#[cfg(windows)]
 mod windows;
-#[cfg(target_os = "windows")]
+#[cfg(windows)]
 pub use self::windows::{dimensions, dimensions_stderr, dimensions_stdin, dimensions_stdout};
 
 // makes project compilable on unsupported platforms
-#[cfg(not(any(
-    target_os = "linux",
-    target_os = "android",
-    target_os = "macos",
-    target_os = "ios",
-    target_os = "bitrig",
-    target_os = "dragonfly",
-    target_os = "freebsd",
-    target_os = "netbsd",
-    target_os = "openbsd",
-    target_os = "solaris",
-    target_os = "windows"
-)))]
+#[cfg(not(any(unix, windows)))]
 mod unsupported;
-#[cfg(not(any(
-    target_os = "linux",
-    target_os = "android",
-    target_os = "macos",
-    target_os = "ios",
-    target_os = "bitrig",
-    target_os = "dragonfly",
-    target_os = "freebsd",
-    target_os = "netbsd",
-    target_os = "openbsd",
-    target_os = "solaris",
-    target_os = "windows"
-)))]
+#[cfg(not(any(unix, windows)))]
 pub use self::unsupported::{dimensions, dimensions_stderr, dimensions_stdin, dimensions_stdout};
